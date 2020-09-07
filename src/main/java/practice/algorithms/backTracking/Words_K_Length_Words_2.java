e7f5a7a2f00a1eb3af31ae0bdb053600587356d4
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2020-09-07 17:04:03 +0530

    words k length words 2

--- src/main/java/practice/algorithms/backTracking/Words_K_Length_Words_2.java
+++ src/main/java/practice/algorithms/backTracking/Words_K_Length_Words_2.java
@@ -1,3 +1,84 @@
+/*
+1. You are given a word (may have one character repeat more than once).
+2. You are given an integer k.
+3. You are required to generate and print all k length words (of distinct chars) by using chars of the
+     word.
+
+Note -> Use the code snippet and follow the algorithm discussed in question video. The judge can't
+               force you but the intention is to teach a concept. Play in spirit of the question.
+Input Format
+Input is managed for you
+Output Format
+Check the sample ouput and question video.
+Constraints
+0 < str.length() < 15
+0 < k <= str.length()
+Sample Input
+aabbbccdde
+3
+Sample Output
+abc
+abd
+abe
+acb
+acd
+ace
+adb
+adc
+ade
+aeb
+aec
+aed
+bac
+bad
+bae
+bca
+bcd
+bce
+bda
+bdc
+bde
+bea
+bec
+bed
+cab
+cad
+cae
+cba
+cbd
+cbe
+cda
+cdb
+cde
+cea
+ceb
+ced
+dab
+dac
+dae
+dba
+dbc
+dbe
+dca
+dcb
+dce
+dea
+deb
+dec
+eab
+eac
+ead
+eba
+ebc
+ebd
+eca
+ecb
+ecd
+eda
+edb
+edc
+ */
+
 package practice.algorithms.backTracking;
 
 import java.io.BufferedReader;
