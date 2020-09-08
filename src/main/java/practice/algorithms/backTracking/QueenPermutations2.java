e4588ec9a2ee81f9c84fdca20d18592d4c73c856
Vinayak Chiluka <chilukavinayak.p@gmail.com>
2020-09-08 20:18:43 +0530

    queen permutation 2d array

---
+++ src/main/java/practice/algorithms/backTracking/QueenPermutations2.java
@@ -0,0 +1,41 @@
+package practice.algorithms.backTracking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class QueenPermutations2 {
+    public static void queensPermutations(int qpsf, int tq, int[][] chess){
+        if(qpsf == tq){
+            for(int i=0;i< chess.length;i++){
+                for(int j=0;j<chess[i].length;j++){
+                    if(chess[i][j] != 0){
+                        System.out.print("q"+chess[i][j]+"\t");
+                    }else
+                        System.out.print("-\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
+
+
+        for(int i=0;i< chess.length;i++){
+            for(int j=0;j<chess[i].length;j++){
+                if(chess[i][j] == 0){
+                    chess[i][j] = qpsf+1;
+                    queensPermutations(qpsf+1,tq,chess);
+                    chess[i][j] = 0;
+                }
+            }
+        }
+
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] chess = new int[n][n];
+
+        queensPermutations(0, n, chess);
+    }
+}
